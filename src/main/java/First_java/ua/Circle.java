2bd86bdebdb97a57360398f11411174a5a7f5e6d
DeshkoM <deshko86@gmail.com>
2022-09-17 17:07:40 +0300

    17.09.2022

--- src/main/java/First_java/ua/Circle.java
+++ src/main/java/First_java/ua/Circle.java
@@ -1,34 +1,61 @@
 package First_java.ua;
 
-public class Circle implements java.io.Serializable{    //Сериализация , требование 1 JavaBeans
+import java.util.Objects;
+
+public class Circle implements java.io.Serializable {    //Сериализация , требование 1 JavaBeans
     private int radius;
-    public int getRadius(){ //приватные переменные и публичные get и set для них, требование 3 JavaBeans
+
+    public int getRadius() { //приватные переменные и публичные get и set для них, требование 3 JavaBeans
         return radius;
     }
-    public void setRadius(int radius){
-        if(radius > 0) {
+
+    public void setRadius(int radius) {
+        if (radius > 0) {
             this.radius = radius;
         }
     }
     //Пустой конструктор , требование 2 JavaBeans
-    public  Circle(){
+    public Circle() {
     }
 
-    public Circle(int radius){
+    public Circle(int radius) {
         this.radius = radius;
     }
 
-    public void perimeterCircle(){
-            double perim = 2 * Math.PI * radius;
-            System.out.println("Периметр круга = " + perim);
+    public String  perimeterCircle() {
+        double perim = 2 * Math.PI * radius;
+        return ("Периметр круга = " + perim);
     }
-    public void areaCircle(){
-            double area = Math.PI * Math.pow(radius, 2);
-            System.out.println("Площадь круга = " + area);
+
+    public String areaCircle() {
+        double area = Math.PI * Math.pow(radius, 2);
+        return ("Площадь круга = " + area);
     }
-    public void msg(){
-            System.out.println("Создан круг с радиусом = " + radius);
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return radius == circle.radius;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(radius);
+    }
+
+    @Override
+    public String toString() {
+        return "Создан круг " +
+                "радиус = " + radius +
+                ' ' +
+                "\n" +
+                perimeterCircle() +
+                " " +
+                "\n" +
+                areaCircle();
     }
 }
-
 
